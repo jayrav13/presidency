f9213ab94e80ac6a8cbb953c949e3644751738c3
Jay Ravaliya <jayrav13@gmail.com>
2016-11-04 01:57:14 -0400

    Initial commits

---
+++ README.md
@@ -0,0 +1,5 @@
+# American Presidency Project
+
+This project is a Python wrapper for the American Presidency Project, found [here](http://www.presidency.ucsb.edu).
+
+More to come!
---
+++ debates/__init__.py
@@ -0,0 +1,106 @@
+from lxml import html
+import requests
+import json
+
+"""
+Debates
+
+A class to access data about debates at a high level.
+"""
+class Debates():
+
+    """
+    __init__
+
+    Initialize by retrieving all data.
+    """
+    def __init__(self):
+        self.data = None
+
+    """
+    all
+
+    Return an array of dicts containing data about all presidential debates.
+    """
+    def all(self):
+
+        url = "http://www.presidency.ucsb.edu/debates.php"
+
+        # Retrieve all debates as tree.
+        page = requests.get(url)
+        tree = html.document_fromstring(page.text)
+
+        # List of all debate and date elements.
+        dates = [x for x in tree.xpath('//td[@class="docdate"]') if len(x.text_content()) > 0]
+        debates = tree.xpath('//td[@class="doctext"]')
+
+        # Throw error if lengths are off.
+        if len(dates) != len(debates):
+            # Throw error.
+            pass
+
+        # Curate list of all debates.
+        self.data = []
+
+        for i in range(0, len(debates)):
+
+            self.data.append({
+                "date" : dates[i].text_content(),
+                "debate" : debates[i].xpath('a')[0].text_content(),
+                "link" : debates[i].xpath('a')[0].attrib['href'],
+                "id" : int(debates[i].xpath('a')[0].attrib['href'].split('?')[1].split('=')[1])
+            })
+
+        return self.data
+
+    """
+    count
+
+    Return number of debates.
+    """
+    def count(self):
+        return len(self.data)
+
+"""
+Debate
+
+A class to access data about a single debate.
+"""
+class Debate():
+
+    """
+    __init__
+
+    Initialize object with an id that can be retrieved from a Deletes class.
+    """
+    def __init__(self, id):
+        self.id = id
+        self.data = []
+        self.retrieve()
+
+    """
+    retrieve
+
+    Return the debate.
+    """
+    def retrieve(self):
+
+        url = 'http://www.presidency.ucsb.edu/ws/index.php?pid='
+
+        page = requests.get(url + str(self.id))
+        tree = html.document_fromstring(page.text)
+
+        self.data = {
+            "text": tree.xpath('//span[@class="displaytext"]')[0].text_content()
+        }
+
+        return self.data
+
+    """
+    words
+
+    Return the (approximate) number of words in this debate.
+    """
+    def words(self):
+        return len(self.data["text"].split(" "))
+
