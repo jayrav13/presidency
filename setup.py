63afcccf7895867042ff8e7bf37e1a006082bd51
Jay Ravaliya <jayrav13@gmail.com>
2016-11-04 13:09:22 -0400

    Including install_requires for dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
     name = 'presidency',
     packages = ['presidency'],
-    version = '0.11',
+    version = '0.13',
     description = 'A Python wrapper for data on the American Presidency Project (www.presidency.ucsb.edu)',
     author = 'Jay Ravaliya',
     author_email = 'jayrav13@gmail.com',
     url = 'https://github.com/jayrav13/presidency',
-    download_url = 'https://github.com/jayrav13/presidency/tarball/0.11',
+    download_url = 'https://github.com/jayrav13/presidency/tarball/0.13',
     keywords = ['wrapper', 'american', 'america', 'presidents', 'president', 'presidency'],
     classifiers = [],
+    install_requires = ['lxml', 'requests']
 )
