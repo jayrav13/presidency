132311156674ce4ad7ea4993870e0dc2aadfdb72
Jay Ravaliya <jayrav13@gmail.com>
2016-11-04 13:19:16 -0400

    Updating to 0.14

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name = 'presidency',
     packages = ['presidency'],
-    version = '0.13',
+    version = '0.14',
     description = 'A Python wrapper for data on the American Presidency Project (www.presidency.ucsb.edu)',
     author = 'Jay Ravaliya',
     author_email = 'jayrav13@gmail.com',
     url = 'https://github.com/jayrav13/presidency',
-    download_url = 'https://github.com/jayrav13/presidency/tarball/0.13',
+    download_url = 'https://github.com/jayrav13/presidency/tarball/0.14',
     keywords = ['wrapper', 'american', 'america', 'presidents', 'president', 'presidency'],
     classifiers = [],
     install_requires = ['lxml', 'requests']
