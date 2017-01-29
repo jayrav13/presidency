d61ea3d8b57fe0d9a5b4676ccc54d6f3c25c192f
Jay Ravaliya <jayrav13@gmail.com>
2017-01-29 17:25:52 -0500

    Updated scraper

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-# Imports
+"""
+Imports
+"""
 from presidency.models import *
 from lxml import html
 import requests
@@ -9,13 +11,16 @@ import os
 import time
 import sys
 
+"""
+Set UTF-8 for everything.
+"""
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 # Establish Base URL.
 base_url = os.environ.get('WHITE_HOUSE_URL') + "/briefing-room/"
 
-# Establish all potential pages.
+# Establish all pages to scrape.
 pages = {
 	"speeches-and-remarks": "Speeches and Remarks",
 	"press-briefings": "Press Briefings",
@@ -28,7 +33,7 @@ pages = {
 	"vetoed-legislation": "Vetoed Legislation"
 }
 
-# Iterate through all pages:
+# Scrape each page.
 for key, value in pages.iteritems():
 	
 	print("Scanning " + value)
@@ -39,33 +44,66 @@ for key, value in pages.iteritems():
 
 	# Deterimine number of total pages.
 	pagecount = int(tree.xpath('//li[@class="pager-current"]')[0].text_content().split(' of ')[1]) if len(tree.xpath('//li[@class="pager-current"]')) > 0 else 1
-
+	print(pagecount)
+	# Keep iterating through pages until you reach a page that has been fully scraped. Then stop.
 	for i in range(0, pagecount):
 
-		# Start pulling pages from the top, this time with the page variable.
-		response = requests.get(base_url + key + "?page=" + str(i))
+		# Use ?page= parameter to scrape, starting with page 0.
+		response = requests.get(base_url + key + "?term_node_tid_depth=&page=" + str(i))
 		tree = html.document_fromstring(response.text)
-		# print(tree.xpath('//div[contains(@class, "views-row")]'))
 
+		# Build the resulting dictionary objects for each document on that page.
 		objects = [{
-			# TODO:
 			"document_date": x.xpath('div[contains(@class, "views-field-created")]')[0].text_content().strip() if len(x.xpath('div[contains(@class, "views-field-created")]')) > 0 else x.xpath('div')[0].text_content().split(' on ')[1],
 			"title": x.xpath('div[contains(@class, "views-field-title")]')[0].text_content().strip(),
 			"uri": x.xpath('div[contains(@class, "views-field-title")]')[0].xpath('h3')[0].xpath('a')[0].attrib['href'].strip(),
 			"category_slug": key,
-			"category_name": value
+			"category_name": value,
+			"full_url": os.environ.get('WHITE_HOUSE_URL') + x.xpath('div[contains(@class, "views-field-title")]')[0].xpath('h3')[0].xpath('a')[0].attrib['href'].strip()
 		} for x in tree.xpath('//div[contains(@class, "views-row")]')]
 
-		records = [WhiteHouse(x['title'], x['uri'], x['category_slug'], x['category_name'], x['document_date']) for x in objects]
+		# Add url's to object.
+		for i in range(0, len(objects)):
+
+			url = requests.post('https://www.googleapis.com/urlshortener/v1/url?key=' + os.environ.get('GOOGLE_URL_SHORTENER_API_KEY'), json={"longUrl": os.environ.get('WHITE_HOUSE_URL') + objects[i]['uri']})
+
+			if url.status_code == 200:
+				objects[i]['short_url'] = url.json()['id']
+			else:
+				objects[i]['short_url'] = objects[i]['short_url']
+
+		# Create database objects for all of these.
+		records = [WhiteHouse(x['title'], x['uri'], x['category_slug'], x['category_name'], x['document_date'], x['full_url'], x['short_url']) for x in objects]
+		
+		# Track number of records successfully added. Those not added will be duplicates.
 		count = 0
+
+		# Iterate through records.
 		for x in records:
+
+			# Attempt to persist.
 			try:
+
 				db.session.add(x)
 				db.session.commit()
-				print("Added " + x.title + " successfully.")
+
 				count += 1
+				
+				print("Added " + x.title + " successfully.")
+
+			# Fallback,
 			except Exception as e:
+
+				# Flush old commit that did not persist.
 				db.session.rollback()
+
+				# Try to save an error message.
+				try:
+					db.session.add(Error(str(e)))
+					db.session.commit()
+				except:
+					db.session.rollback()
+
 				print("Failed to add " + x.title + " successfully: " + str(e))
 
 		# If 0 records were added to the database, everything henceforth is old in this topic.
@@ -73,8 +111,7 @@ for key, value in pages.iteritems():
 		if count == 0:
 			break
 
-# Check if this environment should tweet. If not, exit.
-if not os.environ.get('TWEET_ENV') == 'TRUE':
+if os.environ.get('TWEET_ENV') is not "TRUE":
 	sys.exit()
 
 # Retrieve all documents in descending order.
@@ -88,36 +125,17 @@ twitter = Twython(
 	os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 )
 
+# Go through all relevant documents and tweet them out.
 for document in documents:
-	
-	url = requests.post('https://www.googleapis.com/urlshortener/v1/url?key=' + os.environ.get('GOOGLE_URL_SHORTENER_API_KEY'), json={"longUrl": os.environ.get('WHITE_HOUSE_URL') + document.uri})
-	if url.status_code == 200:
-		url = url.json()['id']
-	else:
-		url = os.environ.get('WHITE_HOUSE_URL') + document.uri
-
-	"""
-	https://support.twitter.com/articles/78124
-
-	All links are resized to 23 characters. Account for this.
-	"""
-	if(len(document.title) < 116): # Taking out the length of URL + 1 space for a space between the two, does this fit?
-		twitter.update_status(status=(document.title + " " + url))
-	else: # Truncate title and add ellipse.
-		print(document.title[0:113] + "... " + url)
-		twitter.update_status(status=(document.title[0: 113] + "... " + url))
-
-	print("Tweeted: " + document.title + " - " + str(document.id))
-
-	document.is_tweeted = True
 
 	try:
-		db.session.add(document)
-		db.session.commit()
-		print("Updated object saved successfully (id: " + str(document.id) + ")")
+		tweet = document.title[0 : 113] + ("..." if len(document.title) < 113 else "") + " " + document.short_url
+		twitter.update_status( status=(tweet) )
 	except Exception as e:
-		db.session.rollback()
-		print("Could not save updated object (id: " + str(document.id) + " - " + str(e))
+		try:
+			db.session.add(Error(str(e)))
+			db.session.commit()
+		except:
+			db.session.rollback()
 
-	print("Sleeping...")
-	time.sleep(10)
+
--- presidency/models.py
+++ presidency/models.py
@@ -1,99 +1,6 @@
 from presidency import app, db
 import datetime
 
-class President(db.Model):
-	"""
-	President
-
-	All Presidents of the United States.
-	"""
-
-	__tablename__ = 'presidents'
-
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
-	name = db.Column(db.String(255))
-	number = db.Column(db.Integer, unique=True)
-
-	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-	updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-
-	documents = db.relationship('Document', backref=db.backref('presidents'))
-
-	def __init__(self, name, number):
-		self.name = name
-		self.number = number
-
-	def to_json(self):
-		output = self.__dict__
-		for key in output:
-			if isinstance(output[key], datetime.datetime):
-				output[key] = str(output[key])
-		output.pop('_sa_instance_state')
-
-		return output
-
-class DocumentCategory(db.Model):
-	"""
-	DocumentCategory
-
-	Written, Oral, etc. + subcategories.
-	"""
-
-	__tablename__ = 'document_categories'
-
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
-	full_text = db.Column(db.String(255))
-	number = db.Column(db.Integer, unique=True)
-	type = db.Column(db.String(255))
-	category = db.Column(db.String(255), nullable=True)
-	subcategory = db.Column(db.String(255), nullable=True)
-
-	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-	updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-
-	documents = db.relationship('Document', backref=db.backref('document_categories'))
-
-	def __init__(self, full_text, number, type, category = None, subcategory = None):
-		self.full_text = full_text
-		self.number = number
-		self.type = type
-		self.category = category
-		self.subcategory = subcategory
-
-	def to_json(self):
-		output = self.__dict__
-		for key in output:
-			if isinstance(output[key], datetime.datetime):
-				output[key] = str(output[key])
-		output.pop('_sa_instance_state')
-
-		return output
-
-class Document(db.Model):
-	"""
-	Document
-
-	A President / DocumentCategory Many-to-Many table to outline all documents.
-	"""
-
-	__tablename__ = 'documents'
-
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
-	pid = db.Column(db.Integer, unique=True, nullable=False)
-	document_date = db.Column(db.DateTime)
-	title = db.Column(db.Text)
-
-	president_id = db.Column(db.Integer, db.ForeignKey('presidents.id'), nullable=False)
-	document_category_id = db.Column(db.Integer, db.ForeignKey('document_categories.id'), nullable=False)
-
-	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-	updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
-
-	def __init__(self, pid, document_date, title):
-		self.pid = pid
-		self.document_date = document_date
-		self.title = title
-
 class WhiteHouse(db.Model):
 	"""
 	WhiteHouse
@@ -111,13 +18,36 @@ class WhiteHouse(db.Model):
 	category_name = db.Column(db.String(255), nullable=True)
 	document_date = db.Column(db.DateTime, nullable=True)
 
+	full_url = db.Column(db.String, nullable=True)
+	short_url = db.Column(db.String, nullable=True)
+
 	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 	updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
-	def __init__(self, title, uri, category_slug=None, category_name=None, document_date=None):
+	def __init__(self, title, uri, category_slug=None, category_name=None, document_date=None, full_url=None, short_url=None):
 		self.title = title
 		self.uri = uri
 		self.category_slug = category_slug
 		self.category_name = category_name
 		self.document_date = datetime.datetime.strptime(document_date, "%B %d, %Y") if document_date != None else document_date
+		self.full_url = full_url
+		self.short_url = short_url
 
+
+class Error(db.Model):
+	"""
+	Error
+
+	Store exceptions for future review.
+	"""
+
+	__tablename__ = 'errors'
+
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	error_message = db.Column(db.Text, nullable=True)
+
+	created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+	updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+
+	def __init__(self, error_message):
+		self.error_message = error_message
