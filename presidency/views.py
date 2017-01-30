aea16a3766e9184c6a4219a18f548a3f4b1762da
Jay Ravaliya <jayrav13@gmail.com>
2017-01-29 22:46:22 -0500

    RSS Feed

--- presidency/views.py
+++ presidency/views.py
@@ -10,6 +10,37 @@ import os
 import requests
 import json
 
+from urlparse import urljoin
+from flask import request
+from werkzeug.contrib.atom import AtomFeed
+
+
+def make_external(url):
+	return urljoin(request.url_root, url)
+
+"""
+/rss
+
+RSS Feed
+"""
+@app.route('/rss')
+def rss_feed():
+
+	feed = AtomFeed('White House Briefing Room Releases', feed_url=request.url, url=request.url_root)
+
+	documents = WhiteHouse.query.all()
+
+	for document in documents:
+
+		feed.add(document.title, document.tweet,
+			content_type='text',
+			author="@presproject2017",
+			url=make_external(document.full_url),
+			updated=document.document_date,
+			published=document.document_date)
+
+	return feed.get_response()
+
 """
 /api/v1/heartbeat
 
